f5364704d39f9bb83828d80fd7e4ac4a8acd292d
Hadi <hadidanial97@gmail.com>
2021-07-11 15:10:16 +0300

    for 2

---
+++ Intro/src/org/tsofen/Loop2.java
@@ -0,0 +1,10 @@
+package org.tsofen;
+
+public class Loop2 {
+public static void main(String[] args) {
+	int min = 0, max = 200;
+	for (int i = min; i <= max; i+=2) {
+		System.out.println(i);
+	}
+}
+}

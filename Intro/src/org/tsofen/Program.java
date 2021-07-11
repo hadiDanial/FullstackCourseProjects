44665beed5b4e29ad8416b97881fd4784bfa52c4
Hadi <hadidanial97@gmail.com>
2021-07-11 13:23:06 +0300

    Exercise 2

--- Intro/src/org/tsofen/Program.java
+++ Intro/src/org/tsofen/Program.java
@@ -4,14 +4,27 @@ import java.util.Scanner;
 
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("Please enter the first number:");
-		int n1, n2;
 		Scanner scanner = new  Scanner(System.in);
-		n1 = scanner.nextInt();
-		System.out.println("Please enter the second number:");
-		n2 = scanner.nextInt();
-		System.out.println("The sum of the two numbers is: " + (n1 + n2));
+//		System.out.println("Hello World!");
+		System.out.println("Please enter the first number:");
+//		int n1, n2;
+//		n1 = scanner.nextInt();
+//		System.out.println("Please enter the second number:");
+//		n2 = scanner.nextInt();
+//		System.out.println("The sum of the two numbers is: " + (n1 + n2));
+		
+		System.out.println("Please enter your name:");
+		String first, last;
+		first = scanner.next();
+		last = scanner.next();
+		System.out.println("Please enter your monthly salary:");
+		float salary = scanner.nextFloat();
+		System.out.println("Hello, " + first + " " + last + "!");
+		System.out.println("Your current salary: " + salary);
+		for (int i = 1; i < 3; i++) {
+			salary = salary * 1.1f;
+			System.out.println("Year " + i + " salary: " + salary);
+		}
 		scanner.close();
 	}
 }

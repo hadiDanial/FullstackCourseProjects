37673d589820a8bbe39ac2db12f0ddb3e60441f0
Hadi <hadidanial97@gmail.com>
2021-07-11 12:14:58 +0300

    First exercise - sum two numbers

--- Intro/src/org/tsofen/Program.java
+++ Intro/src/org/tsofen/Program.java
@@ -1,7 +1,17 @@
 package org.tsofen;
 
+import java.util.Scanner;
+
 public class Program {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Please enter the first number:");
+		int n1, n2;
+		Scanner scanner = new  Scanner(System.in);
+		n1 = scanner.nextInt();
+		System.out.println("Please enter the second number:");
+		n2 = scanner.nextInt();
+		System.out.println("The sum of the two numbers is: " + (n1 + n2));
+		scanner.close();
 	}
 }

5d20fb8ae2576214ea531687ed6cd8453efc2be4
Hadi <hadidanial97@gmail.com>
2021-07-19 12:21:50 +0300

    DB

--- DBConnection/src/connection/DBConnection.java
+++ DBConnection/src/connection/DBConnection.java
@@ -7,14 +7,16 @@ import java.sql.SQLException;
 public class DBConnection
 {
 	private static String driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-	private static String connectionString = " jdbc:sqlserver://127.0.0.1:1433;TsofenDB;integratedSecurity=true";
+	private static String connectionString = "jdbc:sqlserver://127.0.0.1:1433;databaseName=TsofenDB;integratedSecurity=true";
 
 	public DBConnection() throws ClassNotFoundException, SQLException
 	{
-		System.out.println("Loading");
+		System.out.println("Loading Driver");
 		Class.forName(driverClass);
-		System.out.println("Success");
+		System.out.println("Driver Success");
 		
+		System.out.println("Trying to connect");
 		Connection connection = DriverManager.getConnection(connectionString);
+		System.out.println("Connection successful");
 	}
 }
--- DBConnection/src/connection/Program.java
+++ DBConnection/src/connection/Program.java
@@ -1,11 +1,25 @@
 package connection;
 
+import java.sql.SQLException;
+
 public class Program
 {
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		try
+		{
+			DBConnection db = new DBConnection();
+		} catch (ClassNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 	}
 
